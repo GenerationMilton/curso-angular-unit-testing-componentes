09e00a10c291c1085560c29d48016d4c603ebb8d
Milton Muñoz <livemilton@gmail.com>
2024-07-25 23:45:45 -0500

    debug element byCss class

--- src/app/components/person/person.component.spec.ts
+++ src/app/components/person/person.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 import { PersonComponent } from './person.component';
 
 fdescribe('PersonComponent', () => {
@@ -25,10 +27,21 @@ fdescribe('PersonComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  //another test h3
+  it('should have <h3> with "Hola, PersonComponent"',()=>{
+    const personDebug:DebugElement= fixture.debugElement;
+    const h3Debug:DebugElement=personDebug.query(By.css('h3'));
+    const h3: HTMLElement= h3Debug.nativeElement;
+
+    expect(h3?.textContent).toEqual('Hola, PersonComponent');
+  });
+
   //another test
-  it('should hava <p> with "Soy un parrafo"',()=>{
-    const personElement: HTMLElement= fixture.nativeElement;
-    const p = personElement.querySelector('p');
-    expect(p?.textContent).toEqual('Soy un parrafo');
+  it('should have <p> with "Soy un parrafo"',()=>{
+    const personDebug:DebugElement= fixture.debugElement;
+    const pDebug:DebugElement=personDebug.query(By.css('p'));
+    const pElement: HTMLElement= pDebug.nativeElement;
+
+    expect(pElement?.textContent).toEqual('Soy un parrafo');
   });
 });

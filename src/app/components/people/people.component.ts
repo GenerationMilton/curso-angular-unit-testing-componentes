04bf6f960550cde07c69810bc621e54e0e1c0f64
Milton Muñoz <livemilton@gmail.com>
2024-07-27 23:03:51 -0500

    simulate click test

---
+++ src/app/components/people/people.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Person } from 'src/app/models/person.model';
+@Component({
+  selector: 'app-people',
+  templateUrl: './people.component.html',
+  styleUrls: ['./people.component.scss']
+})
+export class PeopleComponent implements OnInit {
+
+  person: Person = new Person('Milton','Munoz',23,1,1);
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/components/person/person.component.spec.ts
+++ src/app/components/person/person.component.spec.ts
@@ -55,15 +55,38 @@ fdescribe('PersonComponent', () => {
   it('should have <p> with "Mi altura es {person.height}"',()=>{
     //Arrange
     component.person= new Person('Valentina','Munoz',28,89,1.4);//Instanciar person
-    const expectMsg=`Mi altura es ${component.person.heigth}`;
+    //const expectMsg=`Mi altura es ${component.person.heigth}`;
     const personDebug:DebugElement= fixture.debugElement;
     const pDebug:DebugElement=personDebug.query(By.css('p'));
     const pElement: HTMLElement= pDebug.nativeElement;
     //Act
     fixture.detectChanges();
     //Assert
-    expect(pElement?.textContent).toContain(expectMsg);
+    expect(pElement?.textContent).toContain(component.person.heigth);
   });
 
+  it('should display a text with IMC when call calcIMC', () => {
+    // Arrange
+    const expectedMsg = 'overweigth level 3';
+    component.person = new Person('Juan', 'Perez', 30, 120, 1.65); // overweight level 3
+    const button = fixture.debugElement.query(By.css('button.btn-imc')).nativeElement;
+    // Act
+    component.calcIMC();
+    fixture.detectChanges();
+    // Assert
+    expect(button.textContent).toContain(expectedMsg);
+  });
 
+  it('should display a text with IMC when do click', () => {
+    // Arrange
+    const expectedMsg = 'overweigth level 3';
+    component.person = new Person('Juan', 'Perez', 30, 120, 1.65); // overweight level 3
+    const buttonDe = fixture.debugElement.query(By.css('button.btn-imc'));
+    const buttonEl = buttonDe.nativeElement;
+    // Act
+    buttonDe.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    // Assert
+    expect(buttonEl.textContent).toContain(expectedMsg);
+  });
 });

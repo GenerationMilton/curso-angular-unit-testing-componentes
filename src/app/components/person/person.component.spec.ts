ccc9819391ec01fce9eb25992d3eedc424d4b700
Milton Muñoz <livemilton@gmail.com>
2024-07-27 22:14:18 -0500

    components with inputs - unit test

--- src/app/components/person/person.component.spec.ts
+++ src/app/components/person/person.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DebugElement } from '@angular/core';
 import { By } from '@angular/platform-browser';
+import { Person } from 'src/app/models/person.model';
 import { PersonComponent } from './person.component';
 
 fdescribe('PersonComponent', () => {
@@ -22,26 +23,47 @@ fdescribe('PersonComponent', () => {
     fixture = TestBed.createComponent(PersonComponent);
     component = fixture.componentInstance;
     fixture.detectChanges(); //ciclo de vida angular
+    component.person= new Person('Milton','Munoz',28,89,1.4);//Instanciar person
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  //test for person
+  it('should the name be "Milton"', ()=>{
+    component.person= new Person('Milton','Munoz',28,89,1.4);//Instanciar person
+    expect(component.person.name).toEqual('Milton');
+  });
+
   //another test h3
-  it('should have <h3> with "Hola, PersonComponent"',()=>{
+  it('should have <h3> with "Hola, {person.name}"',()=>{
+
+    //Arrange
+    component.person= new Person('Valentina','Munoz',28,89,1.4);//Instanciar person
+    const expectMsg=`Hola, ${component.person.name}`;
     const personDebug:DebugElement= fixture.debugElement;
     const h3Debug:DebugElement=personDebug.query(By.css('h3'));
     const h3: HTMLElement= h3Debug.nativeElement;
-
-    expect(h3?.textContent).toEqual('Hola, PersonComponent');
+    //Act
+    fixture.detectChanges();
+    //Assert
+    expect(h3?.textContent).toEqual(expectMsg);
   });
 
   //another test
-  it('should have <p> with "Soy un parrafo"',()=>{
+  it('should have <p> with "Mi altura es {person.height}"',()=>{
+    //Arrange
+    component.person= new Person('Valentina','Munoz',28,89,1.4);//Instanciar person
+    const expectMsg=`Mi altura es ${component.person.heigth}`;
     const personDebug:DebugElement= fixture.debugElement;
     const pDebug:DebugElement=personDebug.query(By.css('p'));
     const pElement: HTMLElement= pDebug.nativeElement;
-
-    expect(pElement?.textContent).toEqual('Soy un parrafo');
+    //Act
+    fixture.detectChanges();
+    //Assert
+    expect(pElement?.textContent).toContain(expectMsg);
   });
+
+
 });
